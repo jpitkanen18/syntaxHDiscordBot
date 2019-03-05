882294cbdcd01611b96759aba852aee5cd4a68d4
Jesse Pitkänen <jpitkanen18@gmail.com>
2019-03-05 18:30:49 +0200

    Updated code to actually work//

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const botConfig = require("./botConfig.json");
-Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./botConfig.json");
+const token = require("./tokenBot.json");
 
-bot.on("ready", async () => {
-    console.log('${bot.user.username} is online!');
-    bot.user.setActivity("Debugging your shit code!");
-});
-bot.on("message", async message => {
-    //Separate commands from arguments.
-    let args = message.content.slice(botConfig.prefix.length).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
 
-    // Ignore commands that don't start with your prefix.
-    if(message.content.indexOf(botConfig.prefix) !== 0) return;
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+client.on("ready", () => {
+	console.log(`I'm alive in ${client.guilds.size} server(s) with a total of ${client.users.size} members.`);
+	client.user.setActivity("BAD_EXEC_ADDRESS");
+	})
+	
+client.on("message", async message => {
+	
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.author.bot) return;
+  
+  if(command === "hello"){
+		message.channel.send("Hello")
+        }
+  if(command === "stobit"){
+      message.channel.send("Nibba stobit :Dddd");
+  }
+		
+}); //END
 
-    let prefix = botConfig.prefix;
-    let messageArray = message.content.split(" ");
-    
-    
-    if(message.content.indexOf(botConfig.prefix) !== 0) return;
-    if(command = `hello`){
-        return message.channel.send("Hello!");
-    }
-});
-
-bot.login(botConfig.token);
+client.login(token.token); //Login.
